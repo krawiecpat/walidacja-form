f9377638cfc1f8c443dab07fbeaa40ea01354024
krawiecpat <krawiecpat9@gmail.com>
2022-01-04 18:01:53 +0100

    walidacja formularza

---
+++ js/script.js
@@ -0,0 +1,63 @@
+console.log('Walidacja zapisu formularza do newslettera');
+
+let newsletterForm = document.getElementById('newsletter-form');
+let allAgreeChx = document.getElementById('all-agree')
+
+const validate = (event) => {
+
+    let txtName = document.getElementById('name');
+    let txtEmail = document.getElementById('email');
+    let agree1 = document.getElementById('agree-1');
+    let errors = document.getElementById('errors');
+
+    errors.innerHTML = '';
+
+    if(txtName.value.trim() === '') {
+        let liError = document.createElement('li');
+        liError.innerText = 'Wpisz Imię i Nazwisko!';
+
+        errors.appendChild(liError)
+    }
+
+    if(txtEmail.value.trim() === '') {
+        let liError = document.createElement('li');
+        liError.innerText = 'Wpisz Adres E-mail!';
+
+        errors.appendChild(liError)
+    }
+
+    if(!txtEmail.value.includes('@')) {
+        let liError = document.createElement('li');
+        liError.innerText = 'Adres E-mail musi zawierać @';
+
+        errors.appendChild(liError)
+    }
+
+    if(!agree1.checked) {
+        let liError = document.createElement('li');
+        liError.innerText = 'Nie wyraźiłeś zgody 1!';
+
+        errors.appendChild(liError)
+    }
+
+    if(errors.children.length > 0){
+        event.preventDefault();
+    }
+    
+}
+
+const allAgree = (event) => {
+    let agree1 = document.getElementById('agree-1');
+    let agree2 = document.getElementById('agree-2');
+    console.log(event.target.checked);
+
+    agree1.checked = event.target.checked;
+    agree2.checked = event.target.checked;
+
+    agree1.disabled = event.target.checked;
+    agree2.disabled = event.target.checked;
+}
+
+newsletterForm.addEventListener('submit', validate);
+
+allAgreeChx.addEventListener('change', allAgree);
